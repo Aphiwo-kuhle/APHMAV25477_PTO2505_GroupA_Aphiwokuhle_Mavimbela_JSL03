36525c374d9fdaf88898f22426bed94e5028b8e8
Aphiwo-kuhle <aphiwokuhlem2223@gmail.com>
2025-10-19 12:30:27 +0200

    deleted unnecessary code

--- test.js
+++ test.js
@@ -143,11 +143,3 @@ showAllTasks();     // Log full board
 showCompletedTasks(); // Log only completed tasks
 
 /* ===================== THE END 😎 ===================== */
-let matrix = [
-  [1, 2, 3],   // row 0
-  [4, 5, 6],   // row 1
-  [7, 8, 9]    // row 2
-];
-
-
-console.log(matrix[1][2]);
